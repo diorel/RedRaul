ec8e48420f0ed0e94db963de0610c5b61f451e45
Diorelyon Amador <diorel_x@hotmail.com>
2018-03-06 13:40:13 -0600

    red con estilos
    se agrego el layout con estilos css

---
+++ RedIncovus/RedSocial/Controllers/DefaultController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RedSocial.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            ViewBag.Variable = "lo que estas enviando";
+            return View();
+        }
+    }
+}
