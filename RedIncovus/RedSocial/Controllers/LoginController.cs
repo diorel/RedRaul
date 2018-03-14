e13594657470677bfb107ccf925890542f61a849
Diorelyon Amador <diorel_x@hotmail.com>
2018-03-13 18:05:24 -0600

    se agrego el metodo guardar en loguin
    se agrego el metodo guardar en loguin

--- RedIncovus/RedSocial/Controllers/LoginController.cs
+++ RedIncovus/RedSocial/Controllers/LoginController.cs
@@ -2,16 +2,51 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Entity;
+using Model;
 using System.Web.Mvc;
+using RedSocial.CustomAttributes;
 
 namespace RedSocial.Controllers
 {
     public class LoginController : Controller
     {
+
+        private UsuarioModel um = new UsuarioModel();
         // GET: Login
         public ActionResult Index()
         {
             return View();
+        }
+
+
+        [HttpPost]
+        [OnlyAjaxRequestAttribute]
+        public JsonResult Acceder(string Correo, string Contrasena)
+        {
+            if (ModelState.IsValid)
+            {
+                return Json(um.Acceder(new Usuario { Correo = Correo, Contrasena = Contrasena }));
+            }
+            else
+            {
+                return Json(new { response = false, message = "Ocurrio un error con la validación del Formulario." });
+            }
+        }
+
+        [HttpPost]
+        [OnlyAjaxRequestAttribute]
+        public JsonResult Registrar(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                return Json(um.RegistroDeLogin(usuario));
+            }
+            else
+            {
+                return Json(new { response = false, message = "Ocurrio un error con la validación del Formulario." });
+            }
+
         }
     }
 }
